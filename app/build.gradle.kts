8c835ceb7624d9f59c5705e42f2dc271dcb100f1
Ruslan Abdrakhmanov <vip.ra.19992@gmail.com>
2026-08-14 19:06:28 +0400

    Версия сборки приведена к номеру релиза, экшен сверяет их перед публикацией;

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "dev.rclaude.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.3.0"
+        versionCode = 5
+        versionName = "0.4.1"
     }
 
     buildTypes {
